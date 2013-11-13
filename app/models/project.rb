f1acf090c08ebb965a6c24c50934fa87393cef17
NanaKwame <easj.asante@gmail.com>
2013-11-13 02:10:32 -0500

    projects basics done. video and paperclip

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,19 @@
 class Project < ActiveRecord::Base
-	 has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+	
+	def youtube_embed
+	  if self.video[/youtu\.be\/([^\?]*)/]
+	    youtube_id = $1
+	  else
+	    # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+	    self.video[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+	    youtube_id = $5
+	  end
+	  if youtube_id
+	  	return "http://www.youtube.com/embed/"+youtube_id 
+	  else
+	  	return ""
+	  end
+	end
 end
