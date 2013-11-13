79e087582e261f9d8b6afcbc680c4d09f0e5d2e6
NanaKwame <easj.asante@gmail.com>
2013-11-12 23:31:14 -0500

    still working

--- db/migrate/20131112074322_create_projects.rb
+++ db/migrate/20131112074322_create_projects.rb
@@ -1,7 +1,11 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-
+      t.string :title
+      t.attachment :image
+      t.string :video
+      t.text :description
+      t.float :goal
       t.timestamps
     end
   end
