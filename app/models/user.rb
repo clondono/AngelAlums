373baff9aedff63725bfad9d457698cb89206363
Christian Londono <clondono@clondono.(none)>
2013-11-17 16:46:55 -0500

    added type to collaborator created user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 end
 
+#method called in order to see if an email belongs to a 
+#certain user. If such a user exists they will be returned
+#if not a new user with be created and that will be returned
 def check_Collaborator(email)
   @user = User.find_by_email(email)
   if @user == nil
-    @user = User.create(email: email, password: Devise.friendly_token[0,20])
+    @user = User.create(email: email, type: "Student", password: Devise.friendly_token[0,20])
     @user.send_reset_password_instructions
   end
   @user
