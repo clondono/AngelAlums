64d2b42d86455463c3ed954d1be3936123bd6897
NanaKwame <easj.asante@gmail.com>
2013-11-19 05:43:24 -0500

    more contingencies

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
 	def index
-		ids = params[:search][:tag_id].reject(&:empty?)
-		@projects = Project.search(ids)
-		@tags = Tag.find(ids)
+		if params[:search]
+			ids = params[:search][:tag_id].reject(&:empty?)
+			@projects = Project.search(ids)
+			@tags = Tag.find(ids)
+		else
+			@projects = []
+			@tags = []
+		end
 	end
 end
