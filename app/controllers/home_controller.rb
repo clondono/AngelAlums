d9f436826b2b06007271e953e5063a8c5698190a
NanaKwame <easj.asante@gmail.com>
2013-12-07 15:36:32 -0500

    still working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,27 @@
 class HomeController < ApplicationController
-  
+  before_action :logged_in
+
   #dashboard for future work ( not mvp)
 
   def index
+    @all_projects = Project.all
+    if current_user.type == "Student"
+      @my_projects = current_user.projects
+      @collab_projects = current_user.shared_projects
+    else
+      @donated_projects = current_user.projects
+    end
+    
 
-  	@projects = current_user.projects
+  end
 
+
+
+  private
+  def logged_in
+    if current_user == nil
+      redirect_to new_user_session_path
+    end
   end
   
 end
