5c42cb33ca4de1c12cb59932103fa9f90b5c4312
NanaKwame <easj.asante@gmail.com>
2013-12-08 18:26:34 -0500

    stuff

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,21 @@
 class SearchController < ApplicationController
-	def index
-		if params[:search]
-			ids = params[:search][:tag_id].reject(&:empty?)
-			@projects = Project.search(ids)
-			@tags = Tag.find(ids)
-		else
-			@projects = []
-			@tags = []
-		end
-	end
+  before_action :logged_in
+
+  def index
+      if params[:search]
+          ids = params[:search][:tag_id].reject(&:empty?)
+          @projects = Project.search(ids)
+          @tags = Tag.find(ids)
+      else
+          @projects = []
+          @tags = []
+      end
+  end
+
+  private
+  def logged_in
+    if current_user == nil
+      redirect_to new_user_session_path, alert: "You are not logged in"
+    end
+  end
 end
