048abc05b80868b7ed208977310c8843728e9d39
NanaKwame <easj.asante@gmail.com>
2013-12-08 22:22:05 -0500

    tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name method" do
+  	assert_equal users(:tony).name , "Tony Stark"
+  end
+
+  test "chack collaboration method" do
+
+  end
+
+  # Most of the stuff for user id handled by devise to no need to test, more information
+  # about devise can be found here https://github.com/plataformatec/devise/wiki.
+
+  # Also all associations are with project model and these associations are tested in the project
+  # model test.
 end
