4865f51c7bbf055d7b97d7558ea44bc03c92c0a8
NanaKwame <easj.asante@gmail.com>
2013-12-08 20:31:37 -0500

    tests

--- test/models/collaboration_test.rb
+++ test/models/collaboration_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CollaborationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a collaborator" do
+    col = Collaboration.create(:name=> "Red", :email => "r@mit.edu")
+    assert (col.name=="Red"), "Collaboration name"
+    assert (col.email=="r@mit.edu"), "Collaboration emial"
+  end
 end
