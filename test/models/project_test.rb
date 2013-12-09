d4139deeeb7c6a6d0b88eeb09269da732dc13b7a
NanaKwame <easj.asante@gmail.com>
2013-12-08 21:50:17 -0500

    tests

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -51,9 +51,8 @@ class ProjectTest < ActiveSupport::TestCase
 
   test "test tag association" do 
   	project = projects(:afri)
-  	project.taggables.create(:tag_id => tags(:home))
-  	assert_equal project.tags.length, 1
-  	project.taggables.create(:tag_id => tags(:blue))
+  	project.taggables.create(:tag_id => tags(:home).id)
+  	project.taggables.create(:tag_id => tags(:blue).id)
   	assert_equal project.tags.length, 2
   end
 
@@ -61,9 +60,37 @@ class ProjectTest < ActiveSupport::TestCase
   	project = users(:gekko).projects.create(:title => "Water", :video => "http://www.youtube.com/watch?v=EzQuwfoeTx8", :description => "water works yeah!", :goal => "200.0")
   	assert_equal project.access_level(users(:gekko).id), "owner"
 
-  	proj = users(:gekko).shared_projects.create(:title => "Water", :video => "http://www.youtube.com/watch?v=EzQuwfoeTx8", :description => "water works yeah!", :goal => "200.0")
-  	assert_equal project.access_level(users(:gekko).id), "collaborator"
+  	proj = users(:budfox).shared_projects.create(:owner_id => users(:gekko).id, :title => "Water", :video => "http://www.youtube.com/watch?v=EzQuwfoeTx8", :description => "water works yeah!", :goal => "200.0")
+  	assert_equal proj.access_level(users(:budfox).id), "collaborator"
 
   	assert_equal project.access_level(users(:tony).id), "none"
+  end
+
+  test "search single result" do
+  	project = projects(:afri)
+  	project.taggables.create(:tag_id => tags(:home).id)
+
+  	results = Project.search(tags(:home).id)
+  	assert_equal results[0].title, projects(:afri).title
+  end
+
+  test "search multiple method" do 
+  	project = projects(:afri)
+  	project.taggables.create(:tag_id => tags(:home).id)
+
+  	proj = projects(:Project_webgame)
+  	proj.taggables.create(:tag_id => tags(:home).id)
+
+
+  	results = Project.search(tags(:home).id)
+  	assert_equal results.length, 2
+  end
+
+  test "number of donators method" do
+  	project = projects(:afri)
+  	project.donations.create( :alum_id => users(:tony).id, :amount => 300)
+  	project.donations.create( :alum_id => users(:tony).id, :amount => 200)
+  	project.donations.create( :alum_id => users(:richard).id, :amount => 200)
+  	assert_equal project.number_of_donators, 2
   end
 end
