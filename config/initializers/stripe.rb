6a328d8582e812e7d15e75803c4e8788323356f2
Richard Park <hs_park@mit.edu>
2013-12-08 23:41:11 -0500

    separate out CSS

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,3 @@
+#Stripe's public and private API keys
 Stripe.api_key = "sk_test_CywUdgUAXqIx5EcToSym3GEr"
 STRIPE_PUBLIC_KEY = "pk_test_eZfnT3GTTvsfV42aAbSMPxC3"
--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,8 @@ Dummy::Application.routes.draw do
     	resources :comments, :only => [:create, :destroy]
     end
   end
-
-
+  
   root 'home#index'
-
-  # Set up a routing for the destroy_all method
-  #match "notes" => 'notes#destroy_all', :via => [:delete]
 
   # Create connections
   match 'admin', to: 'admin#admin', as: 'admin', :via => [:get]
